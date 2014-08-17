bbc8087b66db26fc5d2a1506011385665b26ca38
Jeppe Andersen <jeppe@outlook.com>
2014-08-17 17:00:27 +0200

    Basic tracking entity completed. Supports instantiation from existing entity, tracking and returning a new entity with only modified attributes

---
+++ Src/CrmPowerTools.Tests/TrackingEntityTests.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xrm.Sdk;
+using Xunit;
+
+namespace CrmPowerTools.Tests
+{
+    public class TrackingEntityTests
+    {
+        [Fact]
+        public void TestGetModifiedAttributesOnNewEntityNoAttributesSetReturnsEmptyCollection()
+        {
+            TrackingEntity entity = new TrackingEntity();
+            
+            var modifiesAttributes = entity.GetModifiedAttributes();
+            
+            Assert.Empty(modifiesAttributes);
+        }
+        
+        [Fact]
+        public void TestGetModifiedAttributesSetOneAttributeReturnsOneElement()
+        {
+            TrackingEntity entity = new TrackingEntity();
+            string attribute = "money";
+
+            entity[attribute] = 2.0m;
+            var modifiedAttributes = entity.GetModifiedAttributes();
+
+            Assert.Single(modifiedAttributes);
+            Assert.Equal(attribute, modifiedAttributes.First());
+        }
+
+        [Fact]
+        public void TestGetModifiedAttributesSetMultipleAttributeReturnsAllAttributes()
+        {
+            TrackingEntity entity = new TrackingEntity();
+            var attributes = Enumerable
+                .Range(0, 10)
+                .Select(c => Tuple.Create("Attribute" + c, "Value" + c))
+                .ToList();
+
+            foreach (var attribute in attributes)
+            {
+                entity[attribute.Item1] = attribute.Item2;
+            }
+
+            var modifiedAttributes = entity.GetModifiedAttributes();
+
+            Assert.Equal(attributes.Select(c => c.Item1).ToList(), modifiedAttributes);
+        }
+
+        [Fact]
+        public void TestSetAttributeIndexerOneTimeReturnsValue()
+        {
+            TrackingEntity entity = new TrackingEntity();
+            string attributeName = "attribute";
+            decimal attributeValue = 2.5m;
+
+            entity[attributeName] = attributeValue;
+
+            decimal returnedValue = (decimal) entity[attributeName];
+
+            Assert.Equal(attributeValue, returnedValue);
+        }
+
+        [Fact]
+        public void TestSetAttributeIndexerMultipleTimesReturnsLastValue()
+        {
+            TrackingEntity entity = new TrackingEntity();
+            string attributeName = "attribute";
+            var decimalValues = Enumerable
+                .Range(0, 10)
+                .Select(c => new decimal(c))
+                .ToList();
+
+            foreach (var decimalValue in decimalValues)
+            {
+                entity[attributeName] = decimalValue;
+            }
+
+            decimal returnedValue = (decimal)entity[attributeName];
+
+            Assert.Equal(decimalValues.Last(), returnedValue);
+        }
+
+        [Fact]
+        public void TestGetAttributeIndexerNoAttributeValueThrowsException()
+        {
+            TrackingEntity entity = new TrackingEntity();
+
+            Assert.Throws<KeyNotFoundException>(() => entity["someattribute"]);
+        }
+
+        [Fact]
+        public void TestToFinalEntityOneModifiedAttributeFromExistingReturnsEntityWithOneAttribute()
+        {
+            Entity originalEntity = new Entity();
+            originalEntity["attribute1"] = 2.0m;
+            originalEntity["attribute2"] = "some string value";
+            originalEntity["attribute3"] = true;
+            TrackingEntity entity = new TrackingEntity(originalEntity);
+            entity["attribute1"] = 3.0m;
+
+            Entity finalEntity = entity.ToFinalEntity();
+
+            Assert.Single(finalEntity.Attributes);
+            Assert.Equal("attribute1", finalEntity.Attributes.First().Key);
+            Assert.Equal(3.0m, finalEntity.Attributes.First().Value);
+        }
+
+        [Fact]
+        public void TestToFinalEntityNoModifiedAttributesFromExistingReturnsEntityWithEmptyAttributeCollection()
+        {
+            Entity originalEntity = new Entity();
+            originalEntity["attribute1"] = 5.0m;
+            originalEntity["attribute2"] = "some string value";
+            originalEntity["attribute3"] = false;
+            TrackingEntity entity = new TrackingEntity(originalEntity);
+
+            Entity finalEntity = entity.ToFinalEntity();
+
+            Assert.Empty(finalEntity.Attributes);
+        }
+
+        [Fact]
+        public void TestToFinalEntityMultipleModifiedAttributesFromExistingReturnsEntityWithSameAttributeCollection()
+        {
+            Entity originalEntity = new Entity();
+            originalEntity["attribute1"] = 5.0m;
+            originalEntity["attribute2"] = "some string value";
+            originalEntity["attribute3"] = false;
+            TrackingEntity entity = new TrackingEntity(originalEntity);
+
+            var attributeUpdateValues = new List<KeyValuePair<string, object>>();
+            attributeUpdateValues.Add(new KeyValuePair<string, object>("attribute1", 10.0m));
+            attributeUpdateValues.Add(new KeyValuePair<string, object>("attribute3", false));
+
+            foreach (var attributeUpdateValue in attributeUpdateValues)
+            {
+                entity[attributeUpdateValue.Key] = attributeUpdateValue.Value;
+            }
+
+            Entity finalEntity = entity.ToFinalEntity();
+
+            Assert.Equal(attributeUpdateValues, finalEntity.Attributes);
+        }
+
+    }
+}
---
+++ Src/CrmPowerTools/TrackingEntity.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xrm.Sdk;
+
+namespace CrmPowerTools
+{
+    public class TrackingEntity : Entity
+    {
+        private ISet<string> modifiedAttributes = new HashSet<string>();
+
+        public TrackingEntity() : base()
+        {
+            
+        }
+
+        public TrackingEntity(string entityName) : base(entityName)
+        {
+
+        }
+
+        public TrackingEntity(Entity existing)
+        {
+            this.Id = existing.Id;
+            this.LogicalName = existing.LogicalName;
+            this.EntityState = existing.EntityState;
+            this.Attributes = existing.Attributes;
+            this.ExtensionData = existing.ExtensionData;
+
+            foreach (var relatedEntity in existing.RelatedEntities)
+            {
+                this.RelatedEntities.Add(relatedEntity);
+            }
+
+            foreach (var formattedValue in existing.FormattedValues)
+            {
+                this.FormattedValues.Add(formattedValue);
+            }
+        }
+
+        /// <summary>
+        /// Provides an indexer for the attribute values. If any attribute is set or updated it will be included in the Attributes collection 
+        /// of the resulting entity from invoking the <see cref="ToFinalEntity"/> method.
+        /// </summary>
+        /// <param name="attributeName">The logical name of the attribute.</param>
+        /// <returns>The attribute specified by the attributeName parameter.</returns>
+        public new object this[string attributeName]
+        {
+            get
+            {
+                return this.Attributes[attributeName];    
+            }
+            set
+            {
+                if (!modifiedAttributes.Contains(attributeName))
+                {
+                    modifiedAttributes.Add(attributeName);
+                }
+
+                this.Attributes[attributeName] = value;
+            }   
+        }
+
+        /// <summary>
+        /// Provides a snapshot of all the attributes that has been modified.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<string> GetModifiedAttributes()
+        {
+            return modifiedAttributes.ToList();
+        }
+
+        /// <summary>
+        /// Returns a new entity with only the attributes that have been set or updated.
+        /// </summary>
+        /// <returns></returns>
+        public Entity ToFinalEntity()
+        {
+            Entity finalEntity = ShallowCopyAttributesExcluded(this);
+            foreach (var modifiedAttribute in modifiedAttributes)
+            {
+                finalEntity[modifiedAttribute] = this[modifiedAttribute];
+            }
+
+            return finalEntity;
+        }
+
+        internal Entity ShallowCopyAttributesExcluded(Entity entity)
+        {
+            Entity copy = new Entity();
+
+            copy.Id = entity.Id;
+            copy.LogicalName = entity.LogicalName;
+            copy.EntityState = entity.EntityState;
+            copy.ExtensionData = entity.ExtensionData;
+
+            foreach (var relatedEntity in entity.RelatedEntities)
+            {
+                copy.RelatedEntities.Add(relatedEntity);
+            }
+
+            foreach (var formattedValue in entity.FormattedValues)
+            {
+                copy.FormattedValues.Add(formattedValue);
+            }
+
+            return copy;
+        }
+
+        internal Entity ShallowCopy(Entity entity)
+        {
+            Entity initialCopy = ShallowCopyAttributesExcluded(entity);
+            initialCopy.Attributes = entity.Attributes;
+
+            return initialCopy;
+        }
+
+    }
+}
