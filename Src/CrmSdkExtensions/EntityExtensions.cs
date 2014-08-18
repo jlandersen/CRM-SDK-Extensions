ef7537cf4a91350699c1917ef981f5b410ec9151
Jeppe Andersen <jeppe@outlook.com>
2014-08-18 17:16:59 +0200

    Moved EntityExtensions to correct namespace

---
+++ Src/CrmSdkExtensions/EntityExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xrm.Sdk;
+
+namespace CrmExtensions
+{
+    public static class EntityExtensions
+    {
+        /// <summary>
+        /// Makes a tracking entity from an existing entity that may contain a number of attributes.
+        /// The tracking entity will monitor which attributes are modified.
+        /// </summary>
+        /// <param name="entity">Original CRM entity</param>
+        /// <returns>Tracking version of the original entity</returns>
+        public static TrackingEntity ToTrackingEntity(this Entity entity)
+        {
+            return new TrackingEntity(entity);
+        }
+    }
+}
